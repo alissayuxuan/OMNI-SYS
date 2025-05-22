a42bee6dc34dbc793fb7971cee42df80e2c0fd98
Jer Roy Tan <roy@articly.de>
2025-05-22 15:04:22 +0200

    Create Django object models

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Agent(models.Model):
+    id = models.AutoField(primary_key=True)
+
+class Relationship(models.Model):
+    doctor = models.ForeignKey(Agent, on_delete=models.CASCADE, related_name='patients')
+    patient = models.ForeignKey(Agent, on_delete=models.CASCADE, related_name='doctors')
+
+class Space(models.Model):
+    id = models.AutoField(primary_key=True)
+
+class Context(models.Model):
+    id = models.AutoField(primary_key=True)
+    created = models.DateTimeField(auto_now_add=True)
+    scheduled = models.DateTimeField()
+    space = models.ForeignKey(Space, on_delete=models.SET_NULL)
+    agents = models.ManyToManyField(Agent, related_name='contexts')
