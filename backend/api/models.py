2d95ab6bc61ed0ac81a67f4f29bd88b1695faf1f
Jer Roy Tan <roy@articly.de>
2025-05-22 15:39:36 +0200

    Implement CRUD functions to Django

--- backend/api/models.py
+++ backend/api/models.py
@@ -2,19 +2,25 @@ from django.db import models
 
 # Create your models here.
 
-class Agent(models.Model):
+class OMNISysObject(models.Model):
     id = models.AutoField(primary_key=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        abstract = True
+
+class Agent(OMNISysObject):
+    access_level = models.IntegerField(default=0)
+
+class Space(OMNISysObject):
+    capacity = models.PositiveIntegerField()
+
+class Context(OMNISysObject):
+    scheduled = models.DateTimeField()
+    space = models.ForeignKey(Space, on_delete=models.SET_NULL)
+    agents = models.ManyToManyField(Agent, related_name='contexts')
 
 class Relationship(models.Model):
     doctor = models.ForeignKey(Agent, on_delete=models.CASCADE, related_name='patients')
     patient = models.ForeignKey(Agent, on_delete=models.CASCADE, related_name='doctors')
-
-class Space(models.Model):
-    id = models.AutoField(primary_key=True)
-
-class Context(models.Model):
-    id = models.AutoField(primary_key=True)
-    created = models.DateTimeField(auto_now_add=True)
-    scheduled = models.DateTimeField()
-    space = models.ForeignKey(Space, on_delete=models.SET_NULL)
-    agents = models.ManyToManyField(Agent, related_name='contexts')
--- backend/object_management/database_connector.py
+++ backend/object_management/database_connector.py
@@ -1,18 +1,21 @@
-from object_management.schema import OMNISysObject
-
+from api.models import OMNISysObject
 
 class DatabaseConnector:
     def __init__(self, id_num: int):
         self.id_num = id_num
 
-    def create_object(self, obj: OMNISysObject):
-        pass
+    def create_object(self, obj: OMNISysObject) -> OMNISysObject:
+        return obj.__class__.objects.create(id=obj.id)
 
     def read_object(self, obj: OMNISysObject):
-        pass
+        return obj.objects.get(id=obj.id)
 
     def update_object(self, obj: OMNISysObject):
-        pass
+        object = obj.objects.get(id=obj.id)
+        for f, v in obj.__dict__.items():
+            setattr(object, f, v)
+        object.save()
 
     def delete_object(self, obj: OMNISysObject):
-        pass
+        object = obj.objects.get(id=obj.id)
+        object.delete()
