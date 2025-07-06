58f11b818649fe858c73ca6db2d8442c3623e113
alissayuxuan <alissa.yuxuan@gmail.com>
2025-07-06 22:06:02 +0200

    deleted unnecessary files

--- frontend/src/pages/Register.jsx
+++
@@ -1,7 +0,0 @@
-/*import RegistrationForm from "../components/auth/RegistrationForm"
-
-function Register () {
-    return <RegistrationForm route="/api/auth/user/register/"/>
-}
-
-export default Register*/
