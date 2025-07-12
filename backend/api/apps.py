acb111a1e67d8a08f0b84e2be8f263c1415103eb
ge87dek <ge87dek@mytum.de>
2025-07-13 01:29:08 +0200

    enable rebuilding communication nodes again

--- backend/api/apps.py
+++ backend/api/apps.py
@@ -14,5 +14,5 @@ class ApiConfig(AppConfig):
             from api.models import Agent
             from mqtt_backend.comm_node_manager import CommNodeManager
             agent_ids = list(Agent.objects.filter(is_archived=False).values_list('id', flat=True))
-            #CommNodeManager.rebuild_all(agent_ids)
+            CommNodeManager.rebuild_all(agent_ids)
             print("Rebuilt all comm nodes at backend startup")
